7f0ed9a84512f0de587d64c6d24ea733991f08ae
ricardoamferreira <ricardoandremferreira@gmail.com>
2019-05-14 18:10:54 +0100

    Add first ML process

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import numpy as np
 import create_fingerprints as cf
 import create_descriptors as cd
 from rdkit import Chem
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV, train_test_split, cross_validate
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, matthews_corrcoef
+from sklearn.feature_selection import SelectKBest, f_classif
 
 
 def create_original_df(write=False):
@@ -18,7 +23,7 @@ def create_original_df(write=False):
     if write:
         df_molecules.to_csv("./dataframes/df_molecules.csv")
 
-    return df_molecules
+    return df, df_molecules
 
 
 def createfingerprints(length):
@@ -41,11 +46,34 @@ def createfingerprints(length):
 def createdescriptors():
 
     # Descriptors
-    df_mols_desc = cd.calc_descriptors(df_molecules, True)
+    df_mols_desc = cd.calc_descriptors(df_molecules, False)
 
     return df_mols_desc
 
 
-df_molecules = create_original_df(write=False)
-#ecfp_df, maccs_df, atom_pairs_df, tt_df = createfingerprints(512)
+df, df_molecules = create_original_df(write=False)
+ecfp_df, maccs_df, atom_pairs_df, tt_df = createfingerprints(512)
 df_mols_desc = createdescriptors()
+
+
+#Machine learning process
+#fixing the seed
+seed = 6 # um número qualquer
+np.random.seed(seed)
+
+#Split into X and Y vars
+X = ecfp_df.copy()
+#X = pd.concat([ecfp_df, df_mols_desc], axis=1)
+y = df["Hepatobiliary disorders"].copy()
+
+estimator = SVC(gamma = 0.1)
+scoring_metrics = ['roc_auc', 'accuracy', 'precision', 'recall']
+
+cv_scores = cross_validate(estimator, X, y, scoring=scoring_metrics, cv = 10, return_train_score=False)
+for k, v in cv_scores.items():
+    if k != "fit_time" and k != "score_time":
+        print("Métrica: %s" % k)
+        print("Resultados de cada fold: %s" % v)  # resultados de cada fold
+        print("Média de todas as folds: %s" % np.mean(v))
+        print("Desvio padrão: %s" % np.std(v))
+
